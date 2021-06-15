7d023d012f5a1df5c0b4502766407542d84b6680
Ants Gardiner <antony@wahoofitness.com>
2021-06-15 12:47:17 +1200

    Initial

--- Sources/SwiftZwiftPacketMonitor/SwiftZwiftPacketMonitor.swift
+++ Sources/SwiftZwiftPacketMonitor/SwiftZwiftPacketMonitor.swift
@@ -1,3 +1,21 @@
-struct SwiftZwiftPacketMonitor {
-    var text = "Hello, World!"
+
+import Network
+
+@available(macOS 10.14, *)
+public class SwiftZwiftPacketMonitor {
+
+	/// Default UPD data port for Zwift
+	private let ZWIFT_UDP_PORT = NWEndpoint.Port(rawValue: 3022)!
+
+	/// Default TCP data port for Zwift
+	private let ZWIFT_TCP_PORT = NWEndpoint.Port(rawValue: 3023)!
+
+
+	init() {
+
+
+
+	}
+
+
 }
